6e0c2baf8b687b3e6bee8bac46bacec7f3eb50ba
Michal Kvasnička <m.kv@snicka.cz>
2018-10-03 18:27:47 +0200

    Pridani sassu

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
-export default () => <div>
-    <h1>Ahoj Kutuldo</h1>
-    Hello worldi!</div>
+// styles
+import "./index.scss";
 
+export default () => (
+  <div>
+    <h1>Ahoj Kutuldo</h1>
+    Hello worldi!
+  </div>
+);
