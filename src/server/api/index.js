435c2ba26db3e479e10344a51e4f431f39c9e7e0
Michal Kvasnička <m.kv@snicka.cz>
2018-10-09 19:19:06 +0200

    Odmazani nepouzivaneho loginu

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -7,25 +7,4 @@ const router = express.Router();
 
 router.use("/users", users);
 
-/*
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-
-  if (email === "email@email.cz" && password === "tajneheslo") {
-    res.json({
-      success: true,
-      payload: {
-        redirect: "/protected"
-      }
-    });
-  } else {
-    res.json({
-      success: false,
-      payload: {
-        message: "Chybné přihlašovací údaje"
-      }
-    });
-  }
-});*/
-
 export default router;
